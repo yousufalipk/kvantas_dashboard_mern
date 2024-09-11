d244c2fae04501896a0167476f877b7ae746c520
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-09-11 21:07:05 +0500

    Models import fixed (models)

--- backend/Services/JWTService.js
+++ backend/Services/JWTService.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const RefreshTokenModel = require('../Models/tokenSchema');
+const RefreshTokenModel = require('../models/tokenSchema');
 const dotenv = require('dotenv');
 
 const { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } = require('../config/env');
