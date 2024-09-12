cfbf12f93b2e17e91a391ba54bf55bff6c3fe1b4
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-09-12 10:38:02 +0500

    Changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,34 @@
-const connectToDb = require('./config/db');
-const { PORT } = require('./config/env');
-const app = require('./app');
+const express = require('express')
+const { PORT, FRONTEND_ORIGIN } = require('./config/env');
+const ConnectToDB = require('./config/db');
+const cors = require('cors');
 
-//Db Connection
-connectToDb();
+const userRoutes = require('./routes/userRoutes');
 
+const app = express();
+app.use(express.json());
+
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            return callback(null, true);
+        },
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
+
+
+ConnectToDB();
+
+// test route
+app.get('/', (req, res) => {
+    return res.json('Server Running...')
+})
+
+app.use('/', userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server running on port: ${PORT}`)
+})
